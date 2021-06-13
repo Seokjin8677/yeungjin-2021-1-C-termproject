2b889a1c5fd7265681a0c78a9765705000b4ecc1
leeg0m <hennurunge@naver.com>
2021-06-14 01:03:52 +0900

    이미지 레이어 수정

--- main/ImageLayer.h
+++ main/ImageLayer.h
@@ -2,4 +2,4 @@
 #include "ImageLayerInterface.h"
 #include "ImageLayerImpl.h"
 
-const ImageLayer DEFAULT_IMAGE_LAYER = { NULL, 0, RGB(80,232,34) , NULL, NULL,_initialize, _renderAll, _renderAndFadeIn, _renderAndFadeOut };
+const ImageLayer DEFAULT_IMAGE_LAYER = { NULL, 0, RGB(81,233,34) , NULL, NULL,_initialize, _renderAll, _renderAndFadeIn, _renderAndFadeOut };
