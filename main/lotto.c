e64a5117347633ee57a9f245901de664aeefae36
seokjin8677 <mm8678@g.yju.ac.kr>
2021-06-17 02:44:27 +0900

    로또 출력 변경

--- main/lotto.c
+++ main/lotto.c
@@ -15,6 +15,7 @@ int lotto() {
 	year = tmCurTime.tm_year + 1900;
 	month = tmCurTime.tm_mon + 1;
 	day = tmCurTime.tm_mday;
+
 	for (int i = 0; i < 6; ) {
 		*(comlotto + i) = rand() % 45 + 1;
 		int j;
@@ -36,8 +37,7 @@ int lotto() {
 	}
 	textcolor(15);
 	gotoxy(2, 1); printf("┏━━━━━━━━━━━━━━━━━━━━━━━┓");
-	for (int i = 0; i < 10; i++)
-	{
+	for (int i = 0; i < 10; i++) {
 		gotoxy(2, 2 + i); printf("┃                       ┃");
 	}
 	gotoxy(2, 12); printf("┗━━━━━━━━━━━━━━━━━━━━━━━┛");
@@ -45,45 +45,36 @@ int lotto() {
 	gotoxy(4, 5); printf("발행일:%d/%02d/%02d", year, month, day);
 	gotoxy(4, 6); printf("일렬번호: %d", 10000 + rand() % 50000);
 	gotoxy(4, 7); printf("----------------------");
-
-
 	gotoxy(4, 9); printf("----------------------");
 	gotoxy(4, 11); printf("금액            \\1,000");
+	gotoxy(5, 8);
+	
+	for (int i = 0; i < 7; i++)
+		printf("%2d ", userlotto[i]);
+
 	gotoxy(1, 14);
 	printf("당첨번호: ");
 	for (int i = 0; i < 6; i++)
 	{
-		printf("%d ", comlotto[i]);
-	}
-	gotoxy(5, 8);
-	for (int i = 0; i < 6; i++)
-	{
 		Sleep(500);
 		PlaySound(TEXT("sound\\paper.wav"), NULL, SND_ASYNC);
-
 		for (int j = 0; j < 6; j++)
 		{
-			if (*(userlotto + i) == *(comlotto + j)) {
-
+			if (comlotto[i] == userlotto[j]) {
 				textcolor(12);
 				corretlotto++;
 				break;
 			}
 		}
-		if (*(userlotto + 6) == *(comlotto + i)) {
+		if (comlotto[i] == userlotto[6]) {
+			textcolor(12);
 			corretlotto++;
 			bonus = 1;
 		}
-		printf("%2d ", userlotto[i]);
+		printf("%d ", comlotto[i]);
 		textcolor(15);
 	}
 	Sleep(500);
-	PlaySound(TEXT("sound\\paper.wav"), NULL, SND_ASYNC);
-	if (bonus == 1)
-		textcolor(12);
-	printf("%2d ", userlotto[6]);
-	Sleep(500);
-	textcolor(15);
 	gotoxy(10, 16);
 	switch (corretlotto)
 	{
